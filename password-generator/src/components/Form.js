341e105a4954caf2855e2c0f61c41aca9bbeac77
Filip Warchoł <WarcholF@wp.pl>
2023-02-13 22:12:39 +0100

    Changes to Form component

--- password-generator/src/components/Form.js
+++ password-generator/src/components/Form.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 const Form = () => {
-  let generatedPassword;
   let passwordSymbols = [];
   let length = [8, 16, 20, 24, 28, 32, 64, 128, 256, 512, 1024, 2048];
   let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -95,10 +94,14 @@ const Form = () => {
 
   const [passwordChange, setPasswordChange] = useState("");
   const [checkedNumber, setCheckedNumber] = useState(8);
+  const [includeNumber, setIncludeNumber] = useState(false);
   const [checkedLower, setCheckedLower] = useState(false);
   const [checkedUpper, setCheckedUpper] = useState(false);
   const [checkedSymbols, setCheckedSymbols] = useState(false);
 
+  const handleNumber = () => {
+    setIncludeNumber(!includeNumber);
+  };
   const handleLower = () => setCheckedLower(!checkedLower);
   const handleUpper = () => setCheckedUpper(!checkedUpper);
   const handleSymbols = () => setCheckedSymbols(!checkedSymbols);
@@ -108,8 +111,9 @@ const Form = () => {
   };
 
   const generatePassword = () => {
-    passwordSymbols = passwordSymbols.concat(numbers);
-
+    if (includeNumber) {
+      passwordSymbols = passwordSymbols.concat(numbers);
+    }
     if (checkedLower) {
       passwordSymbols = passwordSymbols.concat(lowerCase);
     }
@@ -118,6 +122,10 @@ const Form = () => {
     }
     if (checkedSymbols) {
       passwordSymbols = passwordSymbols.concat(symbols);
+    }
+    if (!includeNumber & !checkedLower & !checkedUpper & !checkedSymbols) {
+      alert("You need to choose at least one parameter!");
+      return false;
     }
 
     let password = "";
@@ -133,72 +141,87 @@ const Form = () => {
     <div>
       <div class="flex justify-center">
         <div class="flex">
+          <label
+            for="checked-checkbox"
+            class="mt-5  text-sm font-medium text-gray-700 dark:text-gray-700"
+          >
+            Length
+          </label>
           <select
             id="numbers"
-            class=" mx-2 my-2 w-18 bg-gray-50 text-gray-900 text-sm rounded-lg  block  p-2.5 dark:bg-gray-700 dark:border-gray-600  dark:text-white "
+            class=" mx-2 my-2 w-18 bg-gray-500 text-gray-900 text-sm rounded-lg  block  p-2.5 dark:bg-gray-500 dark:border-gray-600  dark:text-white "
             onChange={handleNumberChange}
           >
             {length.map((length) => (
               <option value={length}>{length}</option>
             ))}
           </select>
-          <label
-            for="checked-checkbox"
-            class="mt-5  text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Length
-          </label>
         </div>
         <div class="flex mx-2 my-5">
+          <label
+            for="checked-checkbox"
+            class="mx-2 text-sm font-medium text-gray-900 dark:text-gray-700"
+          >
+            Include Numbers
+          </label>
+          <input
+            id="number-select"
+            type="checkbox"
+            onClick={handleNumber}
+            value=""
+            class="w-4 h-5  bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
+          ></input>
+        </div>
+        <div class="flex mx-2 my-5">
+          <label
+            for="checked-checkbox"
+            class="mx-2 text-sm font-medium text-gray-900 dark:text-gray-700"
+          >
+            Include Lowercase
+          </label>
           <input
             id="default-checkbox"
             type="checkbox"
             onClick={handleLower}
             value=""
-            class="w-4 h-4  bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
+            class="w-4 h-5  bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
           ></input>
-          <label
-            for="checked-checkbox"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Include Lowercase
-          </label>
         </div>
         <div class="flex mx-2 my-5">
+          <label
+            for="checked-checkbox"
+            class="mx-2 text-sm font-medium text-gray-900 dark:text-gray-700"
+          >
+            Include Uppercase
+          </label>
           <input
             id="default-checkbox"
             type="checkbox"
             onClick={handleUpper}
             value=""
-            class="w-4 h-4  bg-gray-100 border-gray-300 rounded  dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
+            class="w-4 h-5  bg-gray-100 border-gray-300 rounded  dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
           ></input>
-          <label
-            for="checked-checkbox"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Include Uppercase
-          </label>
         </div>
         <div class="flex mx-2 my-5">
+          <label
+            for="checked-checkbox"
+            class="mx-2 text-sm font-medium text-gray-900 dark:text-gray-700"
+          >
+            Include Symbols
+          </label>
           <input
             id="default-checkbox"
             type="checkbox"
             onClick={handleSymbols}
             value=""
-            class="w-4 h-4  bg-gray-100 border-gray-300 rounded  dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
+            class="w-4 h-5  bg-gray-100 border-gray-300 rounded  dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
           ></input>
-          <label
-            for="checked-checkbox"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Include Symbols
-          </label>
         </div>
       </div>
       <div class="flex justify-center">
         <button
           type="button"
-          class="py-2.5 px-5 mx-2 mb-2 text-sm font-medium text-white focus:outline-none bg-gray-700 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-white focus:z-10    dark:bg-gray-700 dark:text-white  dark:hover:text-white"
+          class="py-2.5 px-5 mx-2 mb-2 text-sm font-medium text-white focus:outline-none bg-gray-500 rounded-lg focus:z-10  dark:bg-gray-500 dark:text-white  dark:hover:text-white"
           onClick={generatePassword}
         >
           Generate Password
@@ -206,12 +229,12 @@ const Form = () => {
         <input
           type="text"
           id="first_name"
-          class="w-96 py-2.5 px-5 mx-2 mb-2 font-medium bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="w-96 py-2.5 px-5 mx-2 mb-2 font-medium bg-gray-500 text-sm rounded-lg  p-2.5 dark:bg-gray-500  dark:text-white "
           value={passwordChange}
         ></input>
         <button
           type="button"
-          class="py-2.5 px-5 mx-2 mb-2 text-sm font-medium text-white focus:outline-none bg-gray-300 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-white focus:z-10    dark:bg-gray-200 dark:text-white  dark:hover:text-white"
+          class="py-2.5 px-5 mx-2 mb-2 text-sm font-medium  text-white focus:outline-none bg-gray-500 rounded-lg focus:z-10 dark:bg-gray-500 dark:text-white dark:hover:text-white"
           onClick={() => {
             navigator.clipboard.writeText(passwordChange);
           }}
--- password-generator/src/pages/App.js
+++ password-generator/src/pages/App.js
@@ -2,7 +2,7 @@ import Form from "../components/Form";
 
 const MainContent = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-400">
+    <div className="min-h-screen flex flex-col bg-gray-300">
       <Form />
     </div>
   );
