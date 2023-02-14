e0889b272d52c4013eb6a9321d4dc432155196cd
Filip Warchoł <WarcholF@wp.pl>
2023-02-14 12:16:44 +0100

    Footer & Heading components added

---
+++ password-generator/src/components/Heading.js
@@ -0,0 +1,16 @@
+const Heading = () => {
+  return (
+    <div class="block justify-center mt-20 mb-10">
+      <h1 class="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Strong password, secure account
+      </h1>
+      <p class="text-center mb-6 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-900">
+        Generate your unique, strong password to take care of the security of
+        your account, computer, email, program or anything else
+      </p>
+      <hr class="w-96 h-1 mx-auto my-6 bg-gray-600 border-0 rounded md:my-10 dark:bg-gray-600"></hr>
+    </div>
+  );
+};
+
+export default Heading;
--- password-generator/src/pages/App.js
+++ password-generator/src/pages/App.js
@@ -1,9 +1,13 @@
+import Footer from "../components/Footer";
 import Form from "../components/Form";
+import Heading from "../components/Heading";
 
 const MainContent = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-300">
+    <div className="h-screen flex flex-col bg-gray-300">
+      <Heading />
       <Form />
+      <Footer />
     </div>
   );
 };
